1cd80f5605fda4b688174402c64b70c48baf81fb
Berke Sayıl <berkesayil@gmail.com>
2022-02-13 22:44:30 +0300

    Randomized World Intro

--- Assets/Scripts/GridCreator.cs
+++ Assets/Scripts/GridCreator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GridCreator : MonoBehaviour
@@ -10,27 +11,34 @@ public class GridCreator : MonoBehaviour
 
     [SerializeField] int cellSize = 1;
 
+    bool clickedOnGrid = false;
+
+    [SerializeField] //TODO MAKE THIS AUTOMIZED
+    private GameObject[] worldGenBlock = new GameObject[6];
+   
+
     private void Start()
     {
         GenerateGrid();
-
+        
     }
 
     private void GenerateGrid()
     {
-        GameObject referenceTile = (GameObject)Instantiate(Resources.Load("DefaultBlock"));
-
         
 
         for (int row = 0; row < rows; row++)
         {
             for (int col = 0; col < cols; col++)
             {
-                GameObject tile = (GameObject)Instantiate(referenceTile, transform);
+               
+
+                int tileRand = UnityEngine.Random.Range(0, 5);
+                GameObject tile = (GameObject)Instantiate(worldGenBlock[tileRand], transform);
 
                 float posX = col * cellSize;
                 float posY = row * -cellSize;
-                Debug.Log((row, col));
+
                 tile.transform.position = new Vector2(posX, posY);
 
 
@@ -40,9 +48,16 @@ public class GridCreator : MonoBehaviour
         float gridW = cols * cellSize;
         float gridH = rows * cellSize;
 
-        transform.position = new Vector2(gridW / -2 + (cellSize/2) , gridH / 2 - (cellSize/2));
+        float pivot = (float)cellSize / 2;
+        float x = (gridW / -2 + pivot);
+        float y = (gridH / 2 - pivot);
+        
 
-        Destroy(referenceTile);
+        transform.position = new Vector2(x,y);
+
+        
+
+       
     
     }
 
@@ -50,21 +65,12 @@ public class GridCreator : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-
-            if (IsInGrid()) // is mouse in grid?
+            MouseRaycast();
+            if (clickedOnGrid) // is mouse in grid?
             {
 
                 GameObject objectUnderMouse = MouseRaycast();
                 Destroy(objectUnderMouse);
-
-                /*if so and there is a click then place it in the square by centering it
-                Vector2 gridBlock;
-                int gridRow = ((int)(mousePos.x / cellSize));
-                int gridColumn = ((int)(mousePos.y / cellSize));
-
-                gridBlock = new Vector2((gridRow * cellSize) + 2.5f , (gridColumn * cellSize) + 2.5f);
-                */
-
 
             }
             
@@ -85,10 +91,13 @@ public class GridCreator : MonoBehaviour
         if (hit.collider != null)
         {
             //We have reached the object under the mouse
+            clickedOnGrid = true;
             return hit.collider.gameObject;
+            
         }
         else
         {
+            clickedOnGrid = false;
             return null;
         }
     }
@@ -100,11 +109,7 @@ public class GridCreator : MonoBehaviour
 
     }
 
-    private bool IsInGrid()
-    {
-        Vector2 mousePos = MousePosFinder();
-        return mousePos.x > 0 && mousePos.x < (cols * cellSize) && mousePos.y > 0 && mousePos.y < (rows * cellSize);
-    }
+    
 
 
 }
